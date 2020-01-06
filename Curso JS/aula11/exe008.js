1d853337569475d2189bb75dd2a6629180dc26c7
Vinicius Jimenez <vinniciusjimenez@outlook.com>
2020-01-06 19:26:42 -0300

    Aula11 p1

---
+++ Curso JS/aula11/exe008.js
@@ -0,0 +1,10 @@
+var vel = 60.5;
+
+if(vel > 60){
+    console.log(`A velocidade do seu carro é ${vel} km/h`);
+    console.log('Você ultrapassou o limite de 60 Km/h');    
+}
+else{
+    console.log(`A velocidade do seu carro é ${vel} km/h`);
+    console.log('Dirija sempre usando cinto de segurança');
+}
