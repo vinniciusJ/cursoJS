7fd816d3986c78848cb47fc0bf1d82c4c6833d48
Vinicius Jimenez <vinniciusjimenez@outlook.com>
2020-01-07 14:50:01 -0300

    exe014 finished

---
+++ Curso JS/Exercicio14/script014.js
@@ -0,0 +1,25 @@
+function carregar(){
+    let msg = document.querySelector('div#msg');
+    let body = document.querySelector('body');
+    let img = document.querySelector('img#image');
+    //let hora = new Date().getHours();
+
+    let hora = 13;
+
+    if(hora < 12 && hora > 5){
+        msg.innerHTML = `Bom dia!! <br/><br/> Agora são ${hora} horas.`;
+        body.style.background = 'rgba(231, 200, 20, 0.699)';
+        img.src = '_images/manha.jpg';
+    }
+    else if(hora < 18 && hora > 5){
+        msg.innerHTML = `Boa Tarde!! <br/><br/> Agora são ${hora} horas.`;
+        body.style.background = 'rgba(230, 176, 61, 0.897)';
+        img.src = '_images/tarde.jpg';
+    }
+    else if(hora < 24 || hora < 5){
+        msg.innerHTML = `Boa Noite!! <br/><br/> Agora são ${hora} horas.`;
+        body.style.background = 'rgba(58, 14, 116, 0.897)';
+        img.src = '_images/noite.jpg';
+    }
+
+}
