991900b5ea239163485429d932bdcdcab7818492
Vinicius Jimenez <vinniciusjimenez@outlook.com>
2020-01-08 16:07:43 -0300

    aulaVector

---
+++ Curso JS/aula15/ambiente.js
@@ -0,0 +1,5 @@
+let num = [5, 8, 9, 4];
+num.push(8, 1);
+num.sort();
+console.log(`O vetor tem ${num.length} elementos`);
+console.log(`${num}`);
---
+++ Curso JS/aula15/vetorNaTela.js
@@ -0,0 +1,15 @@
+let valores = [50, 5, 3, 5, 8, 9, 192, 1];
+
+for(let i in valores){
+    console.log(`${i + 1}º valor = ${valores[i]}`);
+}
+
+/* Percorrendo Valores
+    for(let i = 0; i < valores.length; i++){
+        
+    }
+*/
+
+/* Método para buscar valores dentro de um arrar - retorna a posição
+    nomeDoVetor.indexOf();
+*/
