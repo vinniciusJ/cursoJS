87655ecd3a920ff110e29700dd62e986d1ec1a5c
Vinicius Jimenez <vinniciusjimenez@outlook.com>
2020-01-08 01:21:58 -0300

    aula13

---
+++ Curso JS/aula13/ambiente.js
@@ -0,0 +1,17 @@
+var contImp = 1;
+
+console.log('Numeros Impares 0-10: ');
+
+while(contImp <= 10){
+    console.log(contImp);
+    contImp+= 2;
+}
+
+var contPar = 0; 
+
+console.log('Numeros Pares 0-10: ');
+
+do{
+    console.log(contPar);
+    contPar+= 2;
+}while(contPar <= 10);
